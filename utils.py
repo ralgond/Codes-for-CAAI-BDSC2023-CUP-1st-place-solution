2d3cd52b1491ebe830f0408d6abd62f6939ab3a7
ralgond <ht201509@163.com>
2023-05-06 12:35:36 +0800

    update

--- utils.py
+++ utils.py
@@ -14,9 +14,10 @@ def set_seed(seed):
     os.environ['PYTHONHASHSEED'] = str(seed)
     random.seed(seed)
     np.random.seed(seed)
+    torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
-        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
         
