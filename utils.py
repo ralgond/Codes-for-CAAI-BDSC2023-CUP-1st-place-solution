977a22e442613f188299275ea39fcef786644c59
Teng Huang <ht201509@163.com>
2023-05-12 22:39:11 +0800

    reproduce.

--- utils.py
+++ utils.py
@@ -20,6 +20,7 @@ def set_seed(seed):
     #     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.enabled = False
         
 def set_logger():
     logging.basicConfig(
