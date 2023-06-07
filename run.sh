4d406a00e451d2b50ccdb9085a42642ae7b45d65
ralgond <ht201509@163.com>
2023-06-07 16:10:53 +0800

    3->4

--- run.sh
+++ run.sh
@@ -23,12 +23,17 @@ if [ ! -f "$the_dir/predict2.txt" ]; then
 	cp save/ecom-social/predict.txt $the_dir/predict2.txt
 fi
 
-# if [ ! -f "$the_dir/predict3.txt" ]; then
-# 	python main.py
-# 	cp save/ecom-social/predict.txt $the_dir/predict3.txt
-# fi
+if [ ! -f "$the_dir/predict3.txt" ]; then
+	python main.py
+	cp save/ecom-social/predict.txt $the_dir/predict3.txt
+fi
 
-python merge_all_predict_files2.py $the_dir
+if [ ! -f "$the_dir/predict4.txt" ]; then
+	python main.py
+	cp save/ecom-social/predict.txt $the_dir/predict4.txt
+fi
+
+python merge_all_predict_files4.py $the_dir
 
 cp $the_dir/predict.txt save/ecom-social/
 
