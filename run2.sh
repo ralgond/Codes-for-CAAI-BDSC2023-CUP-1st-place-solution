de87fcc92fbdcb0baf10c0cada219623709ded4e
Teng Huang <ht201509@163.com>
2023-06-21 17:34:24 +0800

    merge 2 result files.

---
+++ run2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ ! -n "$1" ] ; then
+	the_date=`date +%Y%m%d-%H%M%S`
+	
+	the_dir="save_"${the_date}
+else
+	the_dir="$1"
+fi
+
+
+echo $the_dir
+
+mkdir -p $the_dir
+
+if [ ! -f "$the_dir/predict1.txt" ]; then
+	python main.py
+	cp save/ecom-social/predict.txt $the_dir/predict1.txt
+fi
+
+if [ ! -f "$the_dir/predict2.txt" ]; then
+	python main.py
+	cp save/ecom-social/predict.txt $the_dir/predict2.txt
+fi
+
+
+#python merge_all_predict_files4.py $the_dir
+python merge_all_predict_files2.py $the_dir
+
+cp $the_dir/predict.txt save/ecom-social/
+
+python merge_all.py
